fdf8c128f1c6b350da968a0cdcb95b857ad8d0a1
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-06-23 16:36:57 +1000

    add notes and move loop to new file

--- sample_all_minus_P-A.R
+++ sample_all_minus_P-A.R
@@ -2,19 +2,15 @@
 .libPaths("C:/Users/90946112/R/win-library/3.6.2")
 library(tidyverse)
 
+# load dataset
 flora <- read.delim("data samples/BioNet_allflorasurvey.txt", header = TRUE, sep = "\t", dec = ".")
+
+# select observations with information in 5 select columns- those related to cover estimates/abundance- and records of eucalypt species only
 sample2 <- flora %>% 
   dplyr::filter(is.na(CoverScore)==FALSE|is.na(AbundanceScore)==FALSE|is.na(PercentCover)==FALSE|is.na(LowerHeight)==FALSE|is.na(UpperHeight)==FALSE) %>% 
   dplyr::filter(grepl("Eucalyptus", Assgn_ScientificName))
 sample2 <- droplevels(sample2)
 
-options = list()
-a = 1
-for (i in sample2[,44:48]){
-  b = i
-  options[[a]] = length(b[is.na(b) == FALSE])
-  a = a+1
-  }
-
+# write to csv
 write.csv(sample2,"data samples/all_minus_P-A_data.csv")
 
