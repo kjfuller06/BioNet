e712a1587109fcd49f593439c4d7d7b3f9e6067a
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-07-01 17:19:41 +1000

    Completed all cleaning tasks

--- data_cleaning_all.R
+++ data_cleaning_all.R
@@ -1,37 +1,37 @@
 ## This file is for selecting useful columns and performing some common cleaning processes that could be useful for other scientists at the HIE and outside as well. The workflow is as follows:
 #   1. Load dataset
 #   2. Drop all columns not relevant to (most) species distribution modeling
-#   3. Remove instances where Accuracy is greater than or equal to 1km (1000m according to native units)
-#   4. Remove instances where the start and end dates for an observation are more than 7 days apart
+#   3. Remove all instances except those in which Accuracy is less than 1km (1000m according to native units)
+#   4. Remove all instances except those in which the difference between start and end dates is less than 7 days
 
 # assign library path
 .libPaths("C:/Users/90946112/R/win-library/3.6.2")
 library(tidyverse)
 
 # 1. & 2. ####
-# flora <- read.delim("data samples/BioNet_allflorasurvey.txt", header = TRUE, sep = "\t", dec = ".") %>% 
-#   dplyr::select(Assgn_ScientificName, 
-#                 Exotic, 
-#                 NSWStatus,
-#                 CommStatus,
-#                 SensitivityClass,
-#                 DateFirst,
-#                 DateLast,
-#                 NumberIndividuals,
-#                 EstimateTypeCode,
-#                 SourceCode,
-#                 ObservationType,
-#                 Status,
-#                 Latitude_GDA94,
-#                 Longitude_GDA94,
-#                 Accuracy,
-#                 Stratum,
-#                 GrowthForm,
-#                 CoverScore,
-#                 AbundanceScore,
-#                 PercentCover,
-#                 LowerHeight,
-#                 UpperHeight)
+flora <- read.delim("data samples/BioNet_allflorasurvey.txt", header = TRUE, sep = "\t", dec = ".") %>%
+  dplyr::select(Assgn_ScientificName,
+                Exotic,
+                NSWStatus,
+                CommStatus,
+                SensitivityClass,
+                DateFirst,
+                DateLast,
+                NumberIndividuals,
+                EstimateTypeCode,
+                SourceCode,
+                ObservationType,
+                Status,
+                Latitude_GDA94,
+                Longitude_GDA94,
+                Accuracy,
+                Stratum,
+                GrowthForm,
+                CoverScore,
+                AbundanceScore,
+                PercentCover,
+                LowerHeight,
+                UpperHeight)
 
 # list columns that will be converted to factor variables
 columns=c("Assgn_ScientificName","Exotic","NSWStatus","CommStatus","SensitivityClass","EstimateTypeCode","SourceCode","ObservationType","Status","Stratum","GrowthForm","CoverScore","AbundanceScore")
@@ -46,8 +46,9 @@ flora[c("DateFirst","DateLast")] = lapply(flora[c("DateFirst", "DateLast")], tim
 flora = flora %>% 
   filter(Accuracy < 1000)
 
-backup = flora
-
+# 4. ####
+flora = flora %>% 
+  filter(DateLast - DateFirst < 8)
 
 
 
