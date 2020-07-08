b410a8f4b1bfce41923ad24e536c76aaf50ce517
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-07-08 14:25:30 +1000

    Completed 6 and 7 and export to csv

--- data_cleaning_P-A.R
+++ data_cleaning_P-A.R
@@ -5,6 +5,8 @@
 #   4. Remove all instances except those listed as observation type == "J"- Floristic Record from Systematic Flora Survey
 #   5. Remove all instances except those in which Accuracy is less than or equal to 10m
 #   6. Remove all instances except those originating since 1990
+#   7. Remove columns SourceCode and ObservationType, as these are no longer relevant. And keep only Eucalypt records
+#   8. Write to csv
 
 # assign library path
 .libPaths("C:/Users/90946112/R/win-library/3.6.2")
@@ -12,7 +14,8 @@ library(tidyverse)
 
 # 1. & 2. ####
 flora <- read.csv("data samples/BioNet_allflorasurvey_cleaned.csv", header = TRUE) %>% 
-  dplyr::select(Assgn_ScientificName, 
+  dplyr::select(ID, 
+                Assgn_ScientificName, 
                 DateFirst,
                 DateLast,
                 SourceCode,
@@ -20,8 +23,6 @@ flora <- read.csv("data samples/BioNet_allflorasurvey_cleaned.csv", header = TRU
                 Latitude_GDA94,
                 Longitude_GDA94,
                 Accuracy)
-
-backup1 = flora
 
 # 3. ####
 flora = flora %>% 
@@ -35,11 +36,24 @@ flora = flora %>%
 flora = flora %>% 
   filter(Accuracy <= 10)
 
+# 6. ####
+timefunction <- function(x) as.Date(x, format="%Y-%m-%d")
+flora[c("DateFirst","DateLast")] = lapply(flora[c("DateFirst", "DateLast")], timefunction)
+flora = flora %>% 
+  filter(DateFirst > 1989-12-31)
 
+# 7. ####
+backup = flora
+flora = backup %>% 
+  dplyr::select(ID, 
+                Assgn_ScientificName, 
+                DateFirst,
+                DateLast,
+                Latitude_GDA94,
+                Longitude_GDA94,
+                Accuracy) %>% 
+  filter(grepl("Eucalyptus", Assgn_ScientificName, fixed = TRUE))
 
-
-
-
-
-
+# 8. ####
+write.csv(flora, "data samples/Eucalyptus_presence_absence.csv")
 
