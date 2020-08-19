004cc77010ec1b659270adb926283e34ebbbc87f
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-08-19 17:48:18 -0400

    No library path needed

--- data_cleaning_all.R
+++ data_cleaning_all.R
@@ -9,7 +9,6 @@
 #       -> The user will need to decide how to address apparent duplicates in a given location.
 
 # assign library path
-.libPaths("C:/Users/90946112/R/win-library/3.6.2")
 library(tidyverse)
 
 # 1. & 2. ####
