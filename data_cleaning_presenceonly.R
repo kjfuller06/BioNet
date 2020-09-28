47dc8c536869aedc75da7a449bc579f4dc4398c4
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-09-28 17:05:53 -0500

    Removed all sampling code. Removed st_join with NSW boundary.

--- data_cleaning_presenceonly.R
+++ data_cleaning_presenceonly.R
@@ -5,9 +5,8 @@
 #   4. Remove all instances except those listed as observation type == "J"- Floristic Record from Systematic Flora Survey
 #   5. Remove all instances except those in which Accuracy is less than or equal to 10m
 #   6. Remove all instances except those originating since 1990
-#   7. Remove columns SourceCode and ObservationType, as these are no longer relevant. Keep only records from target species and join bark traits from Bronwyn Horsey's records to occurrence records.
-#   8. Clip records using the boundary of NSW
-#   9. Write to csv
+#   7. Remove columns SourceCode and ObservationType, as these are no longer relevant and remove duplicate records.
+#   8. Write to disk
 
 library(tidyverse)
 library(rnaturalearth)
@@ -50,7 +49,6 @@ flora = flora %>%
 
 # 7. ####
 backup = flora
-sample = read.csv("data samples/Horsey_candidate_speciesV.1.csv")
 flora = backup %>% 
   dplyr::select(ID, 
                 Assgn_ScientificName, 
@@ -59,38 +57,7 @@ flora = backup %>%
                 Latitude_GDA94,
                 Longitude_GDA94) %>% 
   unique()
-flora = flora[flora$Assgn_ScientificName %in% sample$species,] %>% 
-  left_join(sample, by = c("Assgn_ScientificName" = "species"))
 
 # 8. ####
-# get Australia layer
-aus = getData(name = "GADM", country = "AUS", level = 1, download = TRUE) %>% 
-  st_as_sf()
-
-# create NSW layer with ACT included
-nsw = aus %>% 
-  filter(NAME_1 == "New South Wales" | NAME_1 == "Australian Capital Territory")
-
-# create polygon with an extent that hugs the NSW coastline so we can snip off stray islands
-# points are introduced in sequence as they would be drawn on paper, with the last coordinates repeated. So a square will have 5 points.
-bound = list(c( 154, -38), c(140, -38), c( 140, -28), c( 154, -28), c( 154, -38)) %>%
-  unlist() %>%
-  matrix(ncol = 2,
-         byrow = TRUE) %>% 
-  # first convert to a linestring
-  st_linestring %>% 
-  # then convert to a polygon
-  st_cast('POLYGON') %>% 
-  st_sfc(crs = 4326)
-
-# now clip the nsw polygon using st_intersection
-bound = st_intersection(nsw, bound)
-
-# lastly, clip flora records by nsw boundary- minus islands
-flora2 = st_as_sf(flora, coords = c("Longitude_GDA94", "Latitude_GDA94"), crs = 4326) %>% 
-  st_join(bound, join = st_within, left = FALSE)
-
-# 9. ####
-st_write(flora2, "data samples/Horsey_sampleV.1.shp", delete_layer = TRUE)
-st_write(bound, "data samples/NSW_sans_islands.shp", delete_layer = TRUE)
-st_write(aus, "data samples/australia.shp", delete_layer = TRUE)
+# write to disk for use in other scripts
+write.csv(flora, "data samples/BioNet_allfloralsurvey_cleaned2.csv")
